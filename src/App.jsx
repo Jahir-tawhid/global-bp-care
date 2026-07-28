8a6c0e11ef730c60279e78d852119e1b036cc957
Jahir-tawhid <jahirtawhid1988@gmail.com>
2026-07-28 18:56:34 +0600

    Fix duplicate Router issue in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BloodPressurePage from "./pages/BloodPressurePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Route for Blood Pressure App */}
-        <Route path="/" element={<BloodPressurePage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Route for Blood Pressure App */}
+      <Route path="/" element={<BloodPressurePage />} />
+    </Routes>
   );
 }
 
